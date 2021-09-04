7c33d0f3ff484a2be6246f90c44aae3982e42eca
Ronnie.fs.315 <42103638+Thanhtuan96@users.noreply.github.com>
2021-09-04 20:29:27 +0900

    add file font-awsome to project

--- responsive_dev_challenges/interior-consultant-master/app.js
+++ responsive_dev_challenges/interior-consultant-master/app.js
@@ -2,7 +2,5 @@ const toggleBtn = document.querySelector('.btn-toggle');
 const menu = document.querySelector('.menu');
 
 toggleBtn.addEventListener('click', () => {
-    menu.classList.contains('active')
-        ? menu.classList.remove('active')
-        : menu.classList.add('active');
+    menu.classList.toggle('active');
 });
