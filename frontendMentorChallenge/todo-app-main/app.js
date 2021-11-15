a5e0893aaef444b5b3b9a721ccb06d6049097f23
Ronnie.fs.315 <42103638+Thanhtuan96@users.noreply.github.com>
2021-11-15 20:57:20 +0900

    TODO_APP_MAIN: finish basic funtion for todo app... still stuck with check-complete function and display task depend on status

--- frontendMentorChallenge/todo-app-main/app.js
+++ frontendMentorChallenge/todo-app-main/app.js
@@ -5,61 +5,97 @@ const themeToggleBtn = document.querySelector('.btn-theme');
 const desktopPanel = document.querySelector('.desktop-panel');
 const mobilePanel = document.querySelector('.mobile-panel');
 const tasksEmpty = document.querySelector('.tasks-empty');
+const countItem = document.querySelectorAll('.count');
 
-// this function use to check which panel will be shown went create initial todo item
-const pannelCheck = () => {
-    window.innerWidth >= 768
-        ? (desktopPanel.style = 'display: block')
-        : (mobilePanel.style = 'display: block');
-};
+let initialWidth = window.innerWidth;
 
 // this function use to toggel dark theme and light them
 const themeToogle = () => {
-    themeToggleBtn.addEventListener('click', () => {
-        document.querySelector('html').getAttribute('data-theme') === 'dark'
-            ? document.querySelector('html').setAttribute('data-theme', 'light')
-            : document.querySelector('html').setAttribute('data-theme', 'dark');
+    document.querySelector('html').getAttribute('data-theme') === 'dark'
+        ? document.querySelector('html').setAttribute('data-theme', 'light')
+        : document.querySelector('html').setAttribute('data-theme', 'dark');
+};
+// this function use to check which panel will be shown went create initial todo item
+const pannelCheck = (list) => {
+    window.innerWidth >= 768
+        ? (desktopPanel.style = 'display: block')
+        : (mobilePanel.style = 'display: block');
+    if (list.length === 0) {
+        desktopPanel.style = 'display: none';
+        mobilePanel.style = 'display: none';
+    }
+    if (window.innerWidth != initialWidth) {
+        if (window.innerWidth < 768) {
+            if (desktopPanel.style.display == 'block') {
+                desktopPanel.style = 'display: none';
+                mobilePanel.style = 'display: block';
+            }
+        } else {
+            if (mobilePanel.style.display == 'block') {
+                mobilePanel.style = 'display: none';
+                desktopPanel.style = 'display: block';
+            }
+        }
+
+        initialWidth = window.innerWidth;
+    }
+};
+
+// this function to check has any item in localStorage and display
+const checkAnyItem = () => {
+    const list = JSON.parse(localStorage.getItem('tasks'));
+    displayTodoItem(list);
+    pannelCheck(list);
+};
+
+//todo items count
+const updateCountItem = () => {
+    const list = JSON.parse(localStorage.getItem('tasks'));
+    countItem.forEach((item) => {
+        item.textContent = list.length;
     });
 };
 
-themeToogle();
-
-const deleteItemAndRefresh = (list) => {
-    localStorage.setItem('tasks', JSON.stringify(list));
-    displayTodoItem(list);
-};
-
+//this function use to display item from localStorage to DOM
 const displayTodoItem = (list) => {
-    let displayItem = '';
-    if (list === []) {
-        tasksEmpty.style = 'display: block';
+    console.log(list.length);
+    if (list.length === 0) {
+        tasksEmpty.style = 'display: block; border-radius: 8px;';
         todoContainer.style = 'display: none';
+        desktopPanel.style = 'display: none';
+        mobilePanel.style = 'display: none';
     } else {
+        tasksEmpty.style = 'display: none;';
+        todoContainer.style = 'display: block';
+        let displayItem = '';
         list.forEach((item, key) => {
             displayItem += `<div class="todo-block todo-line todo-task" data-id=${key}>
                     <input type="checkbox" class="check-complete">
-                    <p>${item}</p>
+                    <p class='task'>${item}</p>
                     <button class='delete-btn' ></button>
                 </div>`;
             todoContainer.innerHTML = displayItem;
         });
-
-        const deleteBtns = document.querySelectorAll('.todo-task .delete-btn');
-
-        deleteBtns.forEach((btn) => {
-            btn.addEventListener('click', () => {
-                const index = parseInt(
-                    btn.parentElement.getAttribute('data-id')
-                );
-                list.splice(index, 1);
-                console.log(list[0] === null);
-                displayTodoItem(list);
-            });
-        });
     }
+    updateCountItem();
+    pannelCheck(list);
+    deleteItemBtns(list);
+    checkComplete();
 };
 
-const deleteItem = () => {};
+//this function make a delete event btn for each item has been display
+const deleteItemBtns = (list) => {
+    const deleteBtns = document.querySelectorAll('.todo-task .delete-btn');
+    // add delete event for each button
+    deleteBtns.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            const index = parseInt(btn.parentElement.getAttribute('data-id'));
+            list.splice(index, 1);
+            localStorage.setItem('tasks', JSON.stringify(list));
+            checkAnyItem();
+        });
+    });
+};
 
 // create new item and store it to localStorage
 
@@ -75,12 +111,31 @@ const createNewToDoItemAndSave = (e) => {
     displayTodoItem(todoLists);
 };
 
+//this function to make item completed
+const checkComplete = () => {
+    const checkCompleteBtns = document.querySelectorAll('.check-complete');
+    checkCompleteBtns.forEach((item) => {
+        item.addEventListener('click', function () {
+            item.nextElementSibling.classList.toggle('completed');
+            if (item.nextElementSibling.classList.contains('completed')) {
+                item.style = `background: no-repeat url(images/icon-check.svg) center/60%, linear-gradient(to bottom, #57ddff, #c058f3);`;
+            } else item.style = `backgroudn: none`;
+        });
+    });
+};
+
 const todoApp = () => {
+    //get initial item to display into DOM
     let todoLists = JSON.parse(localStorage.getItem('tasks')) || [];
     window.addEventListener('DOMContentLoaded', () => {
         displayTodoItem(todoLists);
     });
+
     todoInput.addEventListener('keyup', createNewToDoItemAndSave);
+
+    themeToggleBtn.addEventListener('click', themeToogle);
+
+    window.addEventListener('resize', pannelCheck);
 };
 
 todoApp();
